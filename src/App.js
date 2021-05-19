55e47eb876c78d5a5248849833d100fe5a12ddd8
kaitlyn-brown <kaitlynbrown_5@hotmail.com>
2021-05-19 08:51:36 -0230

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
